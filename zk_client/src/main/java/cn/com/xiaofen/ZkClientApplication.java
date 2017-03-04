76aaa828e5af5070a06149c23249429cb00402d3
poai <247690790@qq.com>
2017-03-04 19:47:50 +0800

    配置Tomcat数据库连接池,使用Oracle数据库连接

--- zk_client/src/main/java/cn/com/xiaofen/ZkClientApplication.java
+++ zk_client/src/main/java/cn/com/xiaofen/ZkClientApplication.java
@@ -1,13 +1,19 @@
 package cn.com.xiaofen;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 // 启用REST 客户端
@@ -23,5 +29,18 @@ import org.springframework.context.annotation.ImportResource;
 public class ZkClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ZkClientApplication.class, args);
+	}
+
+	/* Spring JDBC */
+	@Bean
+	public JdbcTemplate primaryJdbcTemplate(DataSource dataSource) {
+		return new JdbcTemplate(dataSource);
+	}
+
+	/* TOMCAT DataSourcePool */
+	@Bean
+	@ConfigurationProperties(prefix = "spring.datasource")
+	public DataSource dataSource() {
+		return DataSourceBuilder.create().build();
 	}
 }
--- zk_client/src/main/java/cn/com/xiaofen/controller/HomePage.java
+++ zk_client/src/main/java/cn/com/xiaofen/controller/HomePage.java
@@ -1,5 +1,8 @@
 package cn.com.xiaofen.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,6 +18,16 @@ public class HomePage {
 		bean.setLikes("Play Game!!");
 		bean.setName("Xiao Fen!!");
 		mav.addObject("bean", bean);
+		List<String> list = new ArrayList<String>();
+		for (int i = 0; i < 10; i++) {
+			list.add(i + "");
+		}
+		mav.addObject("list", list);
 		return mav;
+	}
+
+	@RequestMapping("/test_redirect")
+	public String testRedirect() {
+		return "redirect:/index.html";
 	}
 }
